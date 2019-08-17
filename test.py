074f5fe3abbcb01caac75877759214deaf0f0fa0
Manasi Acharya <acharya221b@gmail.com>
2019-08-17 15:12:33 +0530

    Cube Code added in test.

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ def addone(num):
     return num+1
 def square(num):
     return num**2
+def cube(num):
+    return num**3
 if __name__ == "__main__": #to run this file directly using python test.py
     print(addone(4))
     
